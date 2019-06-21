4cb23bb39fd4b9684da10c67675da5c5ad0d8714
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-06-21 16:36:18 -0400

    Add files via upload

--- CURSE/CURSE.py
+++ CURSE/CURSE.py
@@ -49,13 +49,14 @@ def register_course(coursebase, userbase, activeuser):
     temp = input("Input the crn value of the course you want to register :\n")
     for course in coursebase:
         if temp == course.crn:                      # if the course exists attempt to register the active user
-            orary = course.add_student(activeuser[0])
-            if orary:
-                for dude in userbase:
+            orary = course.add_student(activeuser[0]) #this function will check if the student can be added to the course and return a 1 if successfull
+            if orary:      # Execute this code if the student is able to be registered
+                for dude in userbase:           #finds the user in the database
                     if dude == activeuser[0]:
-                        dude.add_course(temp)
+                        dude.add_course(temp)   # adds the crn code to the database
+                        activeuser[0] = dude    # updates the active user data
                         print("\nsuccessfuly registered for:" + temp + "\n")
-                        return
+                        return                  # Stops the method because the task is done
                 print("\nSTUDENT ADDED TO COURSE ROSTER BUT COURSE NOT ADDED TO STUDENT SCHEDULE\nThis should never happen\n")
             return
     print("\nFailed to register\n")
