5dbe93b39c36bf18155d54a720971039af61c38f
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-06-24 15:40:23 -0400

    Add files via upload

--- CURSE/code/Functions/register_course_f.py
+++ CURSE/code/Functions/register_course_f.py
@@ -5,8 +5,8 @@ def register_course(coursebase, userbase, activeuser):
     temp = input("Input the crn value of the course you want to register :\n")
     for course in coursebase:
         if temp == course.crn:                      # if the course exists attempt to register the active user
-            orary = course.add_student(activeuser[0]) #this function will check if the student can be added to the course and return a 1 if successfull
-            if orary:      # Execute this code if the student is able to be registered
+            #this function will check if the student can be added to the course and return a 1 if successfull
+            if course.add_student(activeuser[0]):      # Execute this code if the student is able to be registered
                 for dude in userbase:           #finds the user in the database
                     if dude == activeuser[0]:
                         dude.add_course(temp)   # adds the crn code to the database
