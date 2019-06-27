d017f671e9f361b6f73b0fa6cae4f75861570cc5
Shail  Patel <44775847+patels20@users.noreply.github.com>
2019-06-27 14:15:57 -0400

    Add files via upload

--- CURSE/code/Classes/student_c.py
+++ CURSE/code/Classes/student_c.py
@@ -1,12 +1,13 @@
 from Classes.user_c import *
 class Student(User):
-    def __init__(self, major="CM"):
+    def __init__(self, major="CM", grade=1):
         User.__init__(self, user_id="", first_name="Ben", last_name="Dover", passcode="")
         # initialize the user parts of the student class
 
         self.major = major              # So What's Your Major?
         self.crns = []                  # List of courses that the student is registered in
-        self.course_limit = 4
+        self.course_limit = 4           # Number of courses per semester
+        self.grade = grade              # A students current grade level
 
     def add_course(self,crn_code):
         self.crns.append(crn_code)
