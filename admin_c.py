851df355fdaaeceffe741071a3c7a562dd2487f5
anoufal <44447023+anoufal@users.noreply.github.com>
2019-07-12 19:07:01 -0400

    Delete admin_c.py

--- admin_c.py
+++
@@ -1,5 +0,0 @@
-from Classes.user_c import *
-# Create an inherited class for the admin:
-class Admin(User):
-    def __init__(self):
-        User.__init__(self, user_id="n", first_name="Grande", last_name="Barriga", passcode="n")
