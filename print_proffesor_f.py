ab7d9d2986341c4633a8f809bc2d2f1d80d7d232
anoufal <44447023+anoufal@users.noreply.github.com>
2019-07-12 19:08:14 -0400

    Delete print_proffesor_f.py

--- print_proffesor_f.py
+++
@@ -1,9 +0,0 @@
-from Classes.proffesor_c import *
-
-def print_proffesors(userbase):
-
-    for x in userbase:
-        if x.__class__ == Proffesor:
-            print("\nID: " + x.user_id + "     Name: " + x.first_name + " " + x.last_name)
-
-
